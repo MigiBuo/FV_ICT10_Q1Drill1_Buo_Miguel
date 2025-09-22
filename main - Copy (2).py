6d29e6adbfe1dce48644231248fcaff928ac3e3a
MigiBuo <buo.mge@obmontessori.edu.ph>
2025-09-22 15:36:24 +0800

    Add files via upload

---
+++ main - Copy (2).py
@@ -0,0 +1,9 @@
+from pyscript import display
+
+store_name = "SHELF DISCOVERY" # string
+owner = "Miguel Buo"
+year_founded = "2025"
+
+
+display(store_name,  target= "storeName")
+display(owner,  target= "ownerName")
