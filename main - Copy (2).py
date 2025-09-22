2885f80811db32ddae04ee9ca94ca48abef14d77
MigiBuo <buo.mge@obmontessori.edu.ph>
2025-09-22 15:38:51 +0800

    Delete main - Copy (2).py

--- main - Copy (2).py
+++
@@ -1,9 +0,0 @@
-from pyscript import display
-
-store_name = "SHELF DISCOVERY" # string
-owner = "Miguel Buo"
-year_founded = "2025"
-
-
-display(store_name,  target= "storeName")
-display(owner,  target= "ownerName")
